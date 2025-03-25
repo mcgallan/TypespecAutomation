d01d51c34de63f62e300ee77e39655486a239d31
Tiancheng Zhang (WICRESOFT NORTH AMERICA LTD) <v-zhangtianc@microsoft.com>
2025-03-25 13:52:35 +0800

    fix demo

--- src/emitTypespec.test.ts
+++ src/emitTypespec.test.ts
@@ -26,37 +26,45 @@ test("EmitTypespec-OpenAPI Document", async ({ launch }) => {
     .filter({ hasText: "EmitTypespecProject" })
     .first()
     .click()
-  await sleep(10)
+  await sleep(4)
+  await page.screenshot({ path: "./1.png" })
   await page
     .getByRole("textbox", { name: "input" })
     .fill(">Typespec: Emit From Typespec")
-  await sleep(10)
+  await sleep(4)
+  await page.screenshot({ path: "./2.png" })
 
   await page
     .locator("a")
     .filter({ hasText: "TypeSpec: Emit from TypeSpec" })
     .click()
-  await sleep(10)
+
+  await sleep(4)
+  await page.screenshot({ path: "./3.png" })
+
   await page
     .locator("label div")
     .filter({ hasText: /TextTranslation/ })
     .first()
     .click()
 
-  await sleep(10)
+  await sleep(4)
+  await page.screenshot({ path: "./4.png" })
 
   await page
     .getByRole("option")
     .locator("a")
     .filter({ hasText: /Choose another emitter/ })
     .click()
-  await sleep(10)
+  await sleep(4)
+  await page.screenshot({ path: "./5.png" })
 
   await page
     .locator("a")
     .filter({ hasText: /OpenAPI Document/ })
     .click()
-  await sleep(10)
+  await sleep(4)
+  await page.screenshot({ path: "./6.png" })
 
   await page
     .locator("a")
@@ -68,5 +76,12 @@ test("EmitTypespec-OpenAPI Document", async ({ launch }) => {
     "OpenAPI3...Succeeded",
     "Failed to emit project Successful",
     [5, 2]
+  )
+  await contrastResult(
+    ["openapi.3.0.yaml"],
+    path.resolve(
+      workspacePath,
+      "./Azure.AI.TextTranslation/tsp-output/@typespec/openapi3"
+    )
   )
 })
