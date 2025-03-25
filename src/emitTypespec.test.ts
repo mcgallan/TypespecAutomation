7ae6e1ff08ac9ae0b5eac191a706d32a90fd6da6
Tiancheng Zhang (WICRESOFT NORTH AMERICA LTD) <v-zhangtianc@microsoft.com>
2025-03-25 11:00:37 +0800

    fix emit demo

--- src/emitTypespec.test.ts
+++ src/emitTypespec.test.ts
@@ -26,33 +26,45 @@ test("EmitTypespec-OpenAPI Document", async ({ launch }) => {
     .filter({ hasText: "EmitTypespecProject" })
     .first()
     .click()
+  await sleep(3)
   await page
     .getByRole("textbox", { name: "input" })
     .fill(">Typespec: Emit From Typespec")
+  await sleep(3)
+
   await page
     .locator("a")
     .filter({ hasText: "TypeSpec: Emit from TypeSpec" })
     .click()
+  await sleep(3)
+  await page.pause()
   await page
-    .getByRole("option")
-    .locator("a")
-    .filter({ hasText: /Azure.AI.TextTranslation/ })
-    .first()
+    .locator("label div")
+    .filter({ hasText: /TextTranslation/ })
+    .nth(3)
     .click()
+
+  await sleep(3)
+
   await page
     .getByRole("option")
     .locator("a")
     .filter({ hasText: /Choose another emitter/ })
     .click()
+  await sleep(3)
+
   await page
     .locator("a")
     .filter({ hasText: /OpenAPI Document/ })
     .click()
+  await sleep(3)
 
   await page
     .locator("a")
     .filter({ hasText: /^OpenAPI3$/ })
     .click()
+  await sleep(3)
+
   await preContrastResult(
     page,
     "OpenAPI3...Succeeded",
